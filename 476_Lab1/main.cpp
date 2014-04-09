1604d53038f5ece1b5efac580014d7f9de73cfd1
Twoods01 <twoods0129@gmail.com>
2014-04-09 16:12:26 -0700

    Initial Commit

---
+++ 476_Lab1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  476_Lab1
+//
+//  Created by Taylor Woods on 4/9/14.
+//  Copyright (c) 2014 Taylor Woods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
+
